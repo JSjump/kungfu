2853bd26c3e9eebd463954fa5beb207b7770f636
Zhuyb <919220608@qq.com>
2019-02-24 23:27:27 +0800

    前后台联通，mongoDB已连接

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,15 @@
+import { SET_GAMES} from '../constants';
+export const setGames = (games) => {
+    return {
+        type:SET_GAMES,
+        games
+    }
+}
 export const fetchGames = () => {
     return dispatch => {
         console.log(1111111111)
-        fetch('/api/games');
+        fetch('/api/games')
+            .then(res => res.json())
+            .then(data =>dispatch(setGames(data.games)));
     }
 }
