e105acc7d5fc99341d962fb9b40a83a4fb50f55c
Zybtest <2013230787@qq.com>
2019-03-01 17:04:15 +0800

    s 验证存入数据库  b 表单提交后路由跳转

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import mongodb from 'mongodb';
-
+import bodyParser from 'body-parser';
 const app = express();
+app.use(bodyParser.json());
 const dburl = 'mongodb://localhost';
+const validate = (data) => {
+    let errors = {};
+    if(!data.title)errors.title='Can`t be empty';
+    if(!data.cover)errors.cover='Can not be empty';
+    const isValid = Object.keys(errors).length === 0;
+    return {errors,isValid}
+};
+
 mongodb.MongoClient.connect(dburl,(err,client) => {
      if(err) throw err;
     const db = client.db('crud');
@@ -12,6 +21,26 @@ mongodb.MongoClient.connect(dburl,(err,client) => {
             res.json({games});
         })
     });
+
+    app.post('/api/games',(req,res) => {
+           console.log(req.body)
+        const {errors,isValid} = validate(req.body);
+           if(isValid){
+               const {title,cover} = req.body;
+              db.collection('games').insert(
+                  {title,cover},
+                  (err,result) =>{
+                      if(err){
+                          res.status(500).json({errors:{global: "Something went wrong"}})
+                      }else{
+                          res.json({game:result.ops[0]})
+                      }
+                  }
+              )
+           }else {
+               res.status(400).json({ errors });
+           }
+    })
 
     app.use((req, res) =>{
         res.status(404).json({
--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import classnames from 'classnames';
 import {connect} from "react-redux";
 import {saveGame} from '../actions'
-
+import {Redirect} from 'react-router-dom'
 
 class GameForm extends Component{
     // constructor(props){
@@ -17,6 +17,7 @@ class GameForm extends Component{
         cover:'',
         errors: {},
         loading: false,
+        done:false,
     }
     handleChange = (e) =>{
         if(!!this.state.errors[e.target.name]){
@@ -44,7 +45,7 @@ class GameForm extends Component{
             this.setState({loading: true});
            this.props.saveGame({title,cover})
                .then(
-                   () => {},
+                   () => {this.setState({done:true})},
                    (err) => err.response.json().then(({errors}) => {
                        this.setState({
                            errors,
@@ -57,38 +58,42 @@ class GameForm extends Component{
     }
 
     render() {
-        return(
-            <form className={classnames('ui','form',{loading:this.state.loading})} onSubmit={this.handleSubmit}>
-                <h1>add new game</h1>
-                {!!this.state.errors.global && <div className="ui negative message">{this.state.errors.global}</div>}
-                <div className={classnames('filed',{error:!!this.state.errors.title})}>
-                    <label htmlFor="title">title</label>
-                    <input
-                        type="text"
-                       name="title"
+        const form = <form className={classnames('ui','form',{loading:this.state.loading})} onSubmit={this.handleSubmit}>
+            <h1>add new game</h1>
+            {!!this.state.errors.global && <div className="ui negative message">{this.state.errors.global}</div>}
+            <div className={classnames('filed',{error:!!this.state.errors.title})}>
+                <label htmlFor="title">title</label>
+                <input
+                    type="text"
+                    name="title"
                     value={this.state.title}
                     onChange={this.handleChange}/>
-                    <span>{this.state.errors.title}</span>
-                </div>
+                <span>{this.state.errors.title}</span>
+            </div>
 
-                <div className={classnames('filed',{error:!!this.state.errors.cover})}>
-                    <label htmlFor="cover">cover url</label>
-                    <input
-                        type="text"
-                        name="cover"
-                        value={this.state.cover}
-                        onChange={this.handleChange}/>
-                    <span>{this.state.errors.cover}</span>
-                </div>
+            <div className={classnames('filed',{error:!!this.state.errors.cover})}>
+                <label htmlFor="cover">cover url</label>
+                <input
+                    type="text"
+                    name="cover"
+                    value={this.state.cover}
+                    onChange={this.handleChange}/>
+                <span>{this.state.errors.cover}</span>
+            </div>
 
-                <div className="filed">
-                    {this.state.cover !== '' && <img src={this.state.cover} alt="cover" className="ui small bordered image"/> }
-                    </div>
+            <div className="filed">
+                {this.state.cover !== '' && <img src={this.state.cover} alt="cover" className="ui small bordered image"/> }
+            </div>
 
-                <div className="filed">
-                    <button className="ui primary button">save</button>
-                </div>
-            </form>
+            <div className="filed">
+                <button className="ui primary button">save</button>
+            </div>
+        </form>;
+
+        return(
+            <div>
+            {this.state.done? <Redirect to="/games"></Redirect> : form}
+            </div>
         )
     }
 }
